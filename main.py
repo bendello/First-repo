f585d65b41a8d076616aef6061e5efc721f20b39
bendello <bendello123@gmail.com>
2024-01-08 18:02:35 +0000

    2nd change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 print("Hello World!")
 print("Hello Git")
-side_a = 10
-side_b = 5
-hypotenuse = (side_a**2 + side_b**2)**0.5
+side_a=10
+side_b=5
+hypotenuse=(side_a**2 + side_b**2)**0.5
+print(hypotenuse)
